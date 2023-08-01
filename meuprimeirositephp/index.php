29caa3894934b9361f5e22034e8b157ca909a581
manuelysil <117773644+manuelysil@users.noreply.github.com>
2023-08-01 14:44:21 -0300

    Add files via upload

---
+++ meuprimeirositephp/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "My first PHP script!";
+$turma2 = "vamos desenvolver website em PHP";
+//o comando echo imprime na tela
+echo "<br>".$turma2;
+$x = 5 /* + 15 */ + 5;
+echo "<br>". $x;
+
+$txt = "Hello world!";
+$x = 5;
+$y = 10.5;
+echo "<br>". $txt."x = " .$x. " y = ".$y
+?>
+
+</body>
+</html>
